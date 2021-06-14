70ca31b4010d4d5a3b0d5ad17900e44cac35cd9e
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-06-14 14:15:25 +0900

    Create model User

--- mlog_project/accounts/models.py
+++ mlog_project/accounts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+	handle=models.CharField(max_length=20)
+	biograph=models.TextField(max_length=200)
