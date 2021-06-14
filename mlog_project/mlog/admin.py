5067ae0d4cbb0c24a6cecb22a2a8a189b9f64882
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-06-14 14:29:06 +0900

    Regist models to admin

--- mlog_project/mlog/admin.py
+++ mlog_project/mlog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Genre,SubGenre,Artist,Song,Entry,Like
 
-# Register your models here.
+admin.site.register(Genre)
+admin.site.register(SubGenre)
+admin.site.register(Artist)
+admin.site.register(Song)
+admin.site.register(Entry)
+admin.site.register(Like)
