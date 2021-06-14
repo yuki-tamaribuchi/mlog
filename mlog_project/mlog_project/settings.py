9f37b8bd83fc1313ef9dc757796de1e33ae385f4
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-06-14 14:17:03 +0900

    Add AUTH_USER_MODEL setting

--- mlog_project/mlog_project/settings.py
+++ mlog_project/mlog_project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -111,3 +112,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'accounts.User'
