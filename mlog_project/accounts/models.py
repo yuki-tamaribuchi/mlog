b540b830478d61066e0a5a8b21cba8e5730a9304
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-06-14 14:26:36 +0900

    Create Model Follow

--- mlog_project/accounts/models.py
+++ mlog_project/accounts/models.py
@@ -4,3 +4,13 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
 	handle=models.CharField(max_length=20)
 	biograph=models.TextField(max_length=200)
+
+
+class Follow(models.Model):
+	user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='following_user')
+	follower=models.ForeignKey(User,on_delete=models.CASCADE,related_name='followed_user')
+
+	def __str__(self):
+		return '%s follows %s'%(self.user,self.follower)
+
+
