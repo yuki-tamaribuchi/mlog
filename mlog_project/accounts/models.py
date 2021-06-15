d7c034ae5e4519759d0357aae8e246af8b9d175b
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-06-15 11:17:05 +0900

    Modify Blank setting for user biograph

--- mlog_project/accounts/models.py
+++ mlog_project/accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
 	handle=models.CharField(max_length=20)
-	biograph=models.TextField(max_length=200)
+	biograph=models.TextField(max_length=200,blank=True)
 
 
 class Follow(models.Model):
