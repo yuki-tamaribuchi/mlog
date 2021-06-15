e2f8043c647a78733808844a1ed6c4b920541ab9
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-06-15 13:02:30 +0900

    Create SignUpView

--- mlog_project/accounts/views.py
+++ mlog_project/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .forms import SignUpForm
+
+
+class SignUpView(CreateView):
+	form_class=SignUpForm
+	template_name='accounts/signup.html'
+	success_url='/'
