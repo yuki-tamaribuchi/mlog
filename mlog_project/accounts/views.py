370d1d3154017bdfe548a41329bd3dc829d6e808
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-06-15 15:37:27 +0900

    Create UserDetailView

--- mlog_project/accounts/views.py
+++ mlog_project/accounts/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
-from django.views.generic import CreateView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import CreateView, DetailView
 from django.urls import reverse_lazy
 from django.contrib.auth import login
 from django.http import HttpResponseRedirect
 from django.contrib.auth.views import LoginView as auth_login_view
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import SignUpForm
+from .models import User
 
 
 class SignUpView(CreateView):
@@ -16,3 +18,11 @@ class SignUpView(CreateView):
 
 class LoginView(auth_login_view):
 	template_name='accounts/login.html'
+
+
+class UserDetailView(DetailView):
+	model=User
+	template_name='accounts/userdetail.html'
+
+	def get_object(self):
+		return get_object_or_404(User,username=self.kwargs['username'])
