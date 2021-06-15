0e90b672a34d86f29c28936fc1642f458c92e460
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-06-15 16:17:43 +0900

    Add utl to logout

--- mlog_project/accounts/urls.py
+++ mlog_project/accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 
 from .views import SignUpView, LoginView, UserDetailView
 
@@ -6,5 +7,6 @@ app_name='accounts'
 urlpatterns=[
 	path('signup/',SignUpView.as_view(), name='signup'),
 	path('login/', LoginView.as_view(), name='login'),
+	path('logout/', LogoutView.as_view(), name='logout'),
 	path('detail/<str:username>/', UserDetailView.as_view(), name='detail')
 ]
