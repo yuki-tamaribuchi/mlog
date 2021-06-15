5f750f24e4732ce2a0cf0248a00aa5858f5bf3b6
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-06-15 15:24:53 +0900

    Add url to detail

--- mlog_project/accounts/urls.py
+++ mlog_project/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import SignUpView, LoginView
+from .views import SignUpView, LoginView, UserDetailView
 
 app_name='accounts'
 urlpatterns=[
 	path('signup/',SignUpView.as_view(), name='signup'),
-	path('login/', LoginView.as_view(), name='login')
+	path('login/', LoginView.as_view(), name='login'),
+	path('detail/<str:username>/', UserDetailView.as_view(), name='detail')
 ]
