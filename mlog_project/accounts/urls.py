99c12f64230306c603351c8261ed87d9dcf55279
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2021-06-16 00:40:11 +0900

    Add url to update

--- mlog_project/accounts/urls.py
+++ mlog_project/accounts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
 
-from .views import SignUpView, LoginView, UserDetailView
+from .views import SignUpView, LoginView, UserDetailView, UserUpdateView
 
 app_name='accounts'
 urlpatterns=[
 	path('signup/',SignUpView.as_view(), name='signup'),
 	path('login/', LoginView.as_view(), name='login'),
 	path('logout/', LogoutView.as_view(template_name='accounts/loggedout.html'), name='logout'),
-	path('detail/<str:username>/', UserDetailView.as_view(), name='detail')
+	path('detail/<str:username>/', UserDetailView.as_view(), name='detail'),
+	path('update/', UserUpdateView.as_view(), name='update'),
 ]
