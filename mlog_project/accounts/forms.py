cfb3a8faa5faf74581c6695cba1d4ffda69c7649
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2021-06-16 00:39:10 +0900

    Create UserUpdateForm

--- mlog_project/accounts/forms.py
+++ mlog_project/accounts/forms.py
@@ -1,9 +1,15 @@
 from django.forms import fields
 from .models import User
 from django import forms
-from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm, UserChangeForm
 
 class SignUpForm(UserCreationForm):
 	class Meta:
 		model=User
 		fields=('username','password1','password2','handle','biograph')
+
+
+class UserUpdateForm(UserChangeForm):
+	class Meta:
+		model=User
+		fields=('handle','biograph')
