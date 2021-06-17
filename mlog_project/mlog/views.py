0cfde18b8f2a39261abebd992b229a667a2eca8d
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-06-17 13:03:14 +0900

    Create TimelineView

--- mlog_project/mlog/views.py
+++ mlog_project/mlog/views.py
@@ -2,8 +2,10 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Entry
+from accounts.models import User, Follow
 
 
 class TopView(ListView):
@@ -13,6 +15,21 @@ class TopView(ListView):
 	def get_queryset(self):
 		try:
 			qs=Entry.objects.all().order_by('-id')
+		except ObjectDoesNotExist:
+			qs=Entry.objects.none()
+		return qs
+
+
+class TimelineView(LoginRequiredMixin, ListView):
+	model=Entry
+	template_name='mlog/timeline.html'
+
+	def get_queryset(self):
+		follows=Follow.objects.filter(user__username=self.request.user.username).values('follower__username')
+		
+		try:
+			qs=Entry.objects.filter(writer__username__in=follows)
+			print(qs)
 		except ObjectDoesNotExist:
 			qs=Entry.objects.none()
 		return qs
