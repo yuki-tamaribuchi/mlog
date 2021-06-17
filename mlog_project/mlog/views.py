f189d7f8702b9a35bf5edc80c6a5c9def7b04fa0
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-06-17 09:59:09 +0900

    Create TopView

--- mlog_project/mlog/views.py
+++ mlog_project/mlog/views.py
@@ -1,3 +1,18 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import models
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Entry
+
+
+class TopView(ListView):
+	model=Entry
+	template_name='mlog/topview.html'
+
+	def get_queryset(self):
+		try:
+			qs=Entry.objects.all().order_by('-id')
+		except ObjectDoesNotExist:
+			qs=Entry.objects.none()
+		return qs
