79032e06cf207169026f46a14e3dedbda29257e3
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2021-06-17 15:52:15 +0900

    Add url to comment list

--- mlog_project/mlog/urls.py
+++ mlog_project/mlog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import TopView, TimelineView, EntryDetailView, LikeProcess
+from .views import TopView, TimelineView, EntryDetailView, LikeProcess, CommentListView
 
 app_name='mlog'
 urlpatterns=[
@@ -8,4 +8,5 @@ urlpatterns=[
 	path('timeline/', TimelineView.as_view(), name='timeline'),
 	path('detail/<int:pk>/', EntryDetailView.as_view(), name='detail'),
 	path('likeprocess/', LikeProcess.as_view(), name='likeprocess'),
+	path('detail/<int:pk>/comment/', CommentListView.as_view(), name='commentlist'),
 ]
