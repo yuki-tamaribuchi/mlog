0386b18f1a5204107602035a6788520eb39aa525
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-06-17 21:36:01 +0900

    Create CommentCreateForm

--- mlog_project/mlog/forms.py
+++ mlog_project/mlog/forms.py
@@ -1,10 +1,16 @@
 from django.db.models.base import Model
 from django.forms import ModelForm, fields
 
-from .models import Entry
+from .models import Entry, Comment
 
 
 class EntryCreateForm(ModelForm):
 	class Meta:
 		model=Entry
 		fields=('title','content','song')
+
+
+class CommentCreateForm(ModelForm):
+	class Meta:
+		model=Comment
+		fields=('comment',)
