7c18b910a9dd2bac380f3176eb4ef9039c136c95
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-06-17 13:03:42 +0900

    Add url to timeline

--- mlog_project/mlog/urls.py
+++ mlog_project/mlog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import TopView
+from .views import TopView, TimelineView
 
 app_name='mlog'
 urlpatterns=[
 	path('top/', TopView.as_view(), name='top'),
+	path('timeline/', TimelineView.as_view(), name='timeline'),
 ]
