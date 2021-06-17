b55bd2cd885c0604095533d3bbeabd7982a01783
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2021-06-17 15:17:12 +0900

    Add url to likeprocess

--- mlog_project/mlog/urls.py
+++ mlog_project/mlog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import TopView, TimelineView, EntryDetailView
+from .views import TopView, TimelineView, EntryDetailView, LikeProcess
 
 app_name='mlog'
 urlpatterns=[
 	path('top/', TopView.as_view(), name='top'),
 	path('timeline/', TimelineView.as_view(), name='timeline'),
 	path('detail/<int:pk>/', EntryDetailView.as_view(), name='detail'),
+	path('likeprocess/', LikeProcess.as_view(), name='likeprocess'),
 ]
