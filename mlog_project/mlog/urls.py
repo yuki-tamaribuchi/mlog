3397488d5aeeb72cb25beafe2b92b6af5c557442
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-06-17 13:49:42 +0900

    Add url to entry detail

--- mlog_project/mlog/urls.py
+++ mlog_project/mlog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import TopView, TimelineView
+from .views import TopView, TimelineView, EntryDetailView
 
 app_name='mlog'
 urlpatterns=[
 	path('top/', TopView.as_view(), name='top'),
 	path('timeline/', TimelineView.as_view(), name='timeline'),
+	path('detail/<int:pk>/', EntryDetailView.as_view(), name='detail'),
 ]
