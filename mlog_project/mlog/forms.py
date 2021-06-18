37315a8cb49d482b137ef7a3b8968a890646b3c5
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-06-18 09:53:15 +0900

    Create ArtistCreateForm

--- mlog_project/mlog/forms.py
+++ mlog_project/mlog/forms.py
@@ -1,7 +1,7 @@
 from django.db.models.base import Model
 from django.forms import ModelForm, fields
 
-from .models import Entry, Comment
+from .models import Entry, Comment, Artist, Song
 
 
 class EntryCreateForm(ModelForm):
@@ -14,3 +14,9 @@ class CommentCreateForm(ModelForm):
 	class Meta:
 		model=Comment
 		fields=('comment',)
+
+
+class ArtsitCreateForm(ModelForm):
+	class Meta:
+		model=Artist
+		fields=('artistname','genre','subgenre','artist_name_id','artist_biograph')
