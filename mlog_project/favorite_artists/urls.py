cce11be65470d5a1cc8df9a13d6d3bfde54efb8f
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-06-28 17:33:39 +0900

    Modify appname in urls

--- mlog_project/favorite_artists/urls.py
+++ mlog_project/favorite_artists/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import FavoriteArtistProcess, ArtistFavoriteUserListView, UserFavoriteArtistListView
 
-app_name='favorites'
+app_name='favorite_artists'
 urlpatterns=[
 	path('process/', FavoriteArtistProcess.as_view(), name='process'),
 	path('artist/<str:artist_name_id>/', ArtistFavoriteUserListView.as_view(), name='userfavorite'),
