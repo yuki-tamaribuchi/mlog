996013efde52daddfd965493849df514d35aaeb1
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2021-07-01 15:06:43 +0900

    Add modify size setting

--- mlog_project/utils/utils.py
+++ mlog_project/utils/utils.py
@@ -1,13 +1,25 @@
 def get_profile_image_size(size):
 	PROFILE_IMAGE_SIZE={
+		'XS':{
+			'HEIGHT':50,
+			'WIDTH':50,
+		},
 		'SM':{
 			'HEIGHT':100,
 			'WIDTH':100
 		},
 		'MID':{
+			'HEIGHT':200,
+			'WIDTH':200
+		},
+		'LG':{
 			'HEIGHT':250,
 			'WIDTH':250
-		}
+		},
+		'XLG':{
+			'HEIGHT':300,
+			'WIDTH':300
+		},
 	}
 
 	return PROFILE_IMAGE_SIZE[size]
