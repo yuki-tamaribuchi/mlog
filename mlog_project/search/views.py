7224bd370712d9f226d34b43cf6e3f9cb87dbe16
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-07-01 21:20:46 +0900

    Create BaseSearchListView to DRY

--- mlog_project/search/views.py
+++ mlog_project/search/views.py
@@ -5,58 +5,52 @@ from django.db.models import Q
 from accounts.models import User
 from musics.models import Song, Artist
 
-class ArtistSearchListView(ListView):
-	template_name='search/artist.html'
+
+class BaseSeachListView(ListView):
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['keyword'] = self.request.GET['keyword']
+		return context
+
+
+class ArtistSearchListView(BaseSeachListView):
+	template_name = 'search/artist.html'
 
 	def get_queryset(self):
-		keyword=self.request.GET['keyword']
+		keyword = self.request.GET['keyword']
 
 		if not keyword:
 			return Artist.objects.none()
 
 		return Artist.objects.filter(
-			Q(artist_name__icontains=keyword) | Q(artist_name_id__icontains=keyword)
+			Q(artist_name__icontains = keyword) | Q(artist_name_id__icontains = keyword)
 		)
-
-	def get_context_data(self, **kwargs):
-		context= super().get_context_data(**kwargs)
-		context['keyword']=self.request.GET['keyword']
-		return context
+		
 
 
-class SongSearchListView(ListView):
-	template_name='search/song.html'
+class SongSearchListView(BaseSeachListView):
+	template_name = 'search/song.html'
 
 	def get_queryset(self):
-		keyword=self.request.GET['keyword']
+		keyword = self.request.GET['keyword']
 
 		if not keyword:
 			return Song.objects.none()
 
 		return Song.objects.filter(
-			Q(song_name__icontains=keyword)
+			Q(song_name__icontains = keyword)
 		)
-		
-	def get_context_data(self, **kwargs):
-		context= super().get_context_data(**kwargs)
-		context['keyword']=self.request.GET['keyword']
-		return context
 
 
-class UserSearchListView(ListView):
-	template_name='search/user.html'
+class UserSearchListView(BaseSeachListView):
+	template_name = 'search/user.html'
 
 	def get_queryset(self):
-		keyword=self.request.GET['keyword']
+		keyword = self.request.GET['keyword']
 		
 		if not keyword:
 			return User.objects.none()
 
 		return User.objects.filter(
-			Q(username__icontains=keyword) | Q(handle__icontains=keyword)
+			Q(username__icontains = keyword) | Q(handle__icontains = keyword)
 		)
-
-	def get_context_data(self, **kwargs):
-		context= super().get_context_data(**kwargs)
-		context['keyword']=self.request.GET['keyword']
-		return context
