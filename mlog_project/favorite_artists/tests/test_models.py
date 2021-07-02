b1b1790a630bb5fc3c13717aafef8d25b3237492
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-07-02 13:43:34 +0900

    Create TestFavoriteArtist

---
+++ mlog_project/favorite_artists/tests/test_models.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+
+from favorite_artists.models import FavoriteArtist
+from accounts.models import User
+from musics.models import Artist, Genre
+
+
+class TestFavoriteArtist(TestCase):
+
+	@classmethod
+	def setUp(cls):
+		test_genre = Genre.objects.create(genre_name = 'test genre')
+
+		test_artist = Artist.objects.create(
+			artist_name = 'test artist',
+			artist_name_id = 'testartist',
+		)
+		test_artist.genre.add(test_genre)
+
+		test_user = User.objects.create(
+			username = 'test user',
+			handle = 'testuser',
+		)
+
+		FavoriteArtist.objects.create(
+			user = test_user,
+			artist = test_artist
+		)
+
+	def test_str(self):
+		fav=FavoriteArtist.objects.all().first()
+		self.assertEqual(str(fav), 'test user favorite artist is test artist')
