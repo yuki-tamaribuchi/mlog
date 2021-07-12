b539f62781d4681a499acbf769bd22e95a87fde2
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2021-07-12 14:20:09 +0900

    Create get current user function #4

--- mlog_project/activity/tasks.py
+++ mlog_project/activity/tasks.py
@@ -6,15 +6,17 @@ from accounts.models import User
 from entry.models import Entry
 from musics.models import Artist, Song, Genre
 
+def get_current_user(username):
+	return User.objects.get(username=username)
+
 @app.task()
 def entry_read_activity(pk, username):
 	current_entry = Entry.objects.get(pk=pk)
 
 	if username:
-		current_user = User.objects.get(username=username)
 		models.EntryReadActivity.objects.create(
 			entry=current_entry,
-			user=current_user
+			user=get_current_user(username)
 		)
 	else:
 		models.EntryReadActivity.objects.create(
@@ -26,10 +28,9 @@ def entry_read_activity(pk, username):
 def artist_checked_activity(aritst_name_id, username):
 	if username:
 		current_artist = Artist.objects.get(artist_name_id=aritst_name_id)
-		current_user = User.objects.get(username=username)
 		models.ArtistCheckedActivity.objects.create(
 			artist=current_artist,
-			user=current_user
+			user=get_current_user(username)
 		)
 
 
@@ -37,10 +38,9 @@ def artist_checked_activity(aritst_name_id, username):
 def song_checked_activity(song_pk, username):
 	if username:
 		current_song = Song.objects.get(pk=song_pk)
-		current_user = User.objects.get(username=username)
 		models.SongCheckedActivity.objects.create(
 			song=current_song,
-			user=current_user
+			user=get_current_user(username)
 		)
 
 
@@ -48,18 +48,16 @@ def song_checked_activity(song_pk, username):
 def user_checked_activity(detail_username, current_username):
 	if current_username:
 		detail_user = User.objects.get(username=detail_username)
-		current_user = User.objects.get(username=current_username)
 		models.UserDetailCheckedActivity.objects.create(
 			detail_user=detail_user,
-			user=current_user
+			user=get_current_user(username)
 		)
 
 
 @app.task()
 def genre_checked_activity(genre_name, username):
 	current_genre = Genre.objects.get(genre_name=genre_name)
-	current_user = User.objects.get(username=username)
 	models.GenreCheckedActivity.objects.create(
 		genre=current_genre,
-		user=current_user
+		user=get_current_user(username)
 	)
