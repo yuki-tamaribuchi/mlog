b98419636cd4a962f945ff39a075ca97cf82d941
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-07-20 13:26:36 +0900

    Modify NOSE_ARGS for local and docker

--- mlog_project/mlog_project/settings.py
+++ mlog_project/mlog_project/settings.py
@@ -275,7 +275,16 @@ CELERY_TASK_TIME_LIMIT = 30 * 60
 #Setting for django-nose
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
-NOSE_ARGS = [
-    '--with-xunit',
-    '--xunit-file=result/unittest.xml',
-]
+NOSE_ARGS_LIST = {
+    'local':{
+        '--with-xunit',
+        '--xunit-file=result/unittest.xml',
+    },
+    'docker':{
+        '--with-xunit',
+        '--xunit-file=mlog_project/result/unittest.xml',
+    }
+}
+
+nose_args_selection = os.environ.get('NOSE_ARGS_SELECTION', 'local')
+NOSE_ARGS = NOSE_ARGS_LIST[nose_args_selection]
