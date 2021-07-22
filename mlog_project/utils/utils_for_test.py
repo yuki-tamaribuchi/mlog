f1f52336906edea76c4af52806a10b07919f6cab
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-07-22 10:23:44 +0900

    Add get test genre method

--- mlog_project/utils/utils_for_test.py
+++ mlog_project/utils/utils_for_test.py
@@ -3,6 +3,11 @@ def create_test_genre(genre_name):
 	genre_instance = Genre.objects.create(genre_name = genre_name)
 	return genre_instance
 
+def get_test_genre(gerne_name):
+	from musics.models import Genre
+	genre_instance = Genre.objects.get(gerne_name=gerne_name)
+	return genre_instance
+
 def create_test_artist(artist_name, artist_name_id, genre_name):
 	from musics.models import Artist
 	aritst_instance = Artist.objects.create(
@@ -18,7 +23,7 @@ def create_test_song(song_name, artist_name, artist_name_id, genre_name):
 		song_name = song_name
 	)
 	song_instance.artist.add(create_test_artist(artist_name, artist_name_id ,genre_name))
-	song_instance.genre.add(create_test_genre(genre_name))
+	song_instance.genre.add(get_test_genre(genre_name))
 	return song_instance
 
 def create_test_user(username, handle, biograph):
