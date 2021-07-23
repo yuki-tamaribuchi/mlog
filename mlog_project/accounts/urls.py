f16138f896c4a0f1755b4933196b27ca4e84a9f4
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-07-23 09:12:28 +0900

    Modify codes

--- mlog_project/accounts/urls.py
+++ mlog_project/accounts/urls.py
@@ -1,4 +1,3 @@
-from os import name
 from django.urls import path
 from django.contrib.auth.views import LogoutView
 
@@ -17,16 +16,18 @@ from .views import (SignUpView,
 
 app_name = 'accounts'
 urlpatterns = [
-	path('signup/',SignUpView.as_view(), name = 'signup'),
-	path('login/', LoginView.as_view(), name = 'login'),
-	path('logout/', LogoutView.as_view(template_name = 'accounts/loggedout.html'), name = 'logout'),
-	path('password/', UserPasswordChangeView.as_view(), name = 'password'),
-	path('password/changed/', UserPasswordChangeDoneView.as_view(), name = 'passwordchanged'),
-	path('password/reset/', UserPasswordResetView.as_view(), name = 'passwordreset'),
-	path('password/reset/done/', UserPasswordResetDoneView.as_view(), name = 'passwordresetdone'),
-	path('password/reset/confirm/<uidb64>/<token>/', UserPasswordResetConfirmView.as_view(), name = 'passwordresetconfirm'),
-	path('password/reset/complete/', UserPasswordResetCompleteView.as_view(), name = 'passwordresetcomplete'),
-	path('detail/<str:username>/', UserDetailView.as_view(), name = 'detail'),
-	path('detail/<str:username>/entry/', UserEntryListView.as_view(), name = 'userentrylist'),
-	path('update/', UserUpdateView.as_view(), name = 'update'),
+	path('signup/',SignUpView.as_view(), name='signup'),
+	path('login/', LoginView.as_view(), name='login'),
+	path('logout/', LogoutView.as_view(template_name='accounts/loggedout.html'), name='logout'),
+
+	path('password/', UserPasswordChangeView.as_view(), name='password'),
+	path('password/changed/', UserPasswordChangeDoneView.as_view(), name='passwordchanged'),
+	path('password/reset/', UserPasswordResetView.as_view(), name='passwordreset'),
+	path('password/reset/done/', UserPasswordResetDoneView.as_view(), name='passwordresetdone'),
+	path('password/reset/confirm/<uidb64>/<token>/', UserPasswordResetConfirmView.as_view(), name='passwordresetconfirm'),
+	path('password/reset/complete/', UserPasswordResetCompleteView.as_view(), name='passwordresetcomplete'),
+	
+	path('detail/<str:username>/', UserDetailView.as_view(), name='detail'),
+	path('detail/<str:username>/entry/', UserEntryListView.as_view(), name='userentrylist'),
+	path('update/', UserUpdateView.as_view(), name='update'),
 ]
