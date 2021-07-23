d053a747f19cfb67d837f65764c6c159d911b013
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-07-23 14:15:28 +0900

    Modify

--- mlog_project/entry/views.py
+++ mlog_project/entry/views.py
@@ -16,36 +16,36 @@ from activity.tasks import entry_read_activity
 
 
 class EntryDetailView(DetailView):
-	template_name='entry/detail.html'
+	template_name = 'entry/detail.html'
 
 	def get_object(self):
-		current_entry=get_object_or_404(Entry,pk=self.kwargs['pk'])
-
+		current_entry = get_object_or_404(Entry,pk=self.kwargs['pk'])
 		entry_read_activity.delay(self.kwargs['pk'], self.request.user.username)
 
 		return current_entry
 
+
 	def get_context_data(self, **kwargs):
-		context=super().get_context_data(**kwargs)
-		context['like_count']=Like.objects.filter(entry=self.kwargs['pk']).count()
-		context['comment_count']=Comment.objects.filter(entry=self.kwargs['pk']).count()
+		context = super().get_context_data(**kwargs)
+		context['like_count'] = Like.objects.filter(entry=self.kwargs['pk']).count()
+		context['comment_count'] = Comment.objects.filter(entry=self.kwargs['pk']).count()
 		
 		try:
-			context['like_status']=Like.objects.filter(user__username=self.request.user.username,entry=self.kwargs['pk'])
+			context['like_status'] = Like.objects.filter(user__username=self.request.user.username,entry=self.kwargs['pk'])
 		except ObjectDoesNotExist:
-			context['like_status']=Like.objects.none()
+			context['like_status'] = Like.objects.none()
 
-		context['view_count']=EntryReadActivity.objects.filter(entry__id=self.kwargs['pk']).count()
+		context['view_count'] = EntryReadActivity.objects.filter(entry__id=self.kwargs['pk']).count()
 
 		return context
 
 
 class EntryCreateView(LoginRequiredMixin,CreateView):
 	form_class=EntryCreateForm
-	template_name='entry/create.html'
+	template_name = 'entry/create.html'
 
 	def form_valid(self, form):
-		form.instance.writer_id=User.objects.get(username=self.request.user.username).id
+		form.instance.writer_id = User.objects.get(username=self.request.user.username).id
 		return super().form_valid(form)
 
 	def get_success_url(self):
@@ -53,15 +53,15 @@ class EntryCreateView(LoginRequiredMixin,CreateView):
 
 
 class EntryUpdateView(LoginRequiredMixin, UpdateView):
-	template_name='entry/update.html'
-	fields=('title','content','song')
+	template_name = 'entry/update.html'
+	fields = ('title','content','song')
 
 	def get_object(self):
 		return Entry.objects.get(writer__username=self.request.user.username,id=self.kwargs['pk'])
 
 
 class EntryDeleteView(LoginRequiredMixin, DeleteView):
-	template_name='entry/delete_confirm.html'
+	template_name = 'entry/delete_confirm.html'
 
 	def get_object(self):
 		return Entry.objects.get(writer__username=self.request.user.username,id=self.kwargs['pk'])
