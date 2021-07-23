5ebc4020c42fd5cf74c1138169c2a331e83a3686
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-07-23 10:14:17 +0900

    Modify

--- mlog_project/comments/models.py
+++ mlog_project/comments/models.py
@@ -4,9 +4,9 @@ from accounts.models import User
 from entry.models import Entry
 
 class Comment(models.Model):
-	comment=models.TextField(max_length=200)
-	entry=models.ForeignKey(Entry, on_delete=models.CASCADE)
-	user=models.ForeignKey(User, on_delete=models.CASCADE)
+	comment = models.TextField(max_length=200)
+	entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 	def __str__(self):
-		return '%s comment to %s'%(self.user.username,self.entry.title)
+		return '%s comment to %s'%(self.user.username, self.entry.title)
