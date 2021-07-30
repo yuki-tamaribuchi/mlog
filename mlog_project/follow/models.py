7784ba97e083e33e9da96d8f4e19d38c8446d7ea
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-07-30 13:41:27 +0900

    Modify

--- mlog_project/follow/models.py
+++ mlog_project/follow/models.py
@@ -4,8 +4,8 @@ from accounts.models import User
 
 
 class Follow(models.Model):
-	user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='following_user')
-	follower = models.ForeignKey(User,on_delete=models.CASCADE,related_name='followed_user')
+	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following_user')
+	follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followed_user')
 
 	def __str__(self):
 		return '%s follows %s'%(self.user,self.follower)
