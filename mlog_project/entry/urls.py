9f6b425f860f6c599b84ea030d2e0ae0b4aec7b5
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-08-03 08:31:27 +0900

    Add url to EntryListBySongView

--- mlog_project/entry/urls.py
+++ mlog_project/entry/urls.py
@@ -5,6 +5,7 @@ from .views import (
 	EntryCreateView,
 	EntryUpdateView,
 	EntryDeleteView,
+	EntryListBySongView,
 )
 
 app_name='entry'
@@ -14,6 +15,8 @@ urlpatterns=[
 	path('detail/<int:pk>/', EntryDetailView.as_view(), name='detail'),
 	path('update/<int:pk>/', EntryUpdateView.as_view(), name='update'),
 	path('delete/<int:pk>/', EntryDeleteView.as_view(), name='delete'),
+
+	path('list/song/<int:pk>/', EntryListBySongView.as_view(), name='entry_list_by_song'),
 
 	path('select2/', include('django_select2.urls')),
 ]
