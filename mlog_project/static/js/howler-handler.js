00bc1eace69e991d28051d3e2befb9ab7eff8a13
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2021-08-15 10:00:07 +0900

    Move howler function to howler-handler.js

---
+++ mlog_project/static/js/howler-handler.js
@@ -0,0 +1,28 @@
+spotify_preview_url = document.currentScript.getAttribute('spotify_preview_url');
+
+var sound = new Howl({
+	src: [spotify_preview_url],
+	format:['mp3']
+});
+
+function play_pause(sound){
+	if (sound.playing()){
+		$('#id_artwork_for_preview_in_entry_detail').removeClass('artwork_circle');
+		sound.pause();
+		$('.circle-wrap .circle .mask.full, .circle-wrap .circle .fill').css("animation-play-state", "paused");
+		$('.pause_icon').remove();
+		$('.inside-circle').append('<i class="fas fa-play fa-2x play_icon"></i>');
+	}else{
+		$('#id_artwork_for_preview_in_entry_detail').addClass('artwork_circle');
+		sound.play();
+		$('.circle-wrap .circle .mask.full, .circle-wrap .circle .fill').css("animation-play-state", "running");
+		$('.play_icon').remove();
+		$('.inside-circle').append('<i class="fas fa-pause fa-2x pause_icon"></i>');
+
+	}
+
+	sound.on('end', function(){
+		$('.circle-wrap .circle .mask.full, .circle-wrap .circle .fill').css("animation-play-state", "paused");
+		$('#id_artwork_for_preview_in_entry_detail').removeClass('artwork_circle');
+	})
+}
