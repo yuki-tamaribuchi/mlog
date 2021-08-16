cafd37364e979b7d9cb852caee6149d27595756a
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-08-16 11:39:55 +0900

    Move add genre function to add_genre.js

--- mlog_project/static/js/add_genre.js
+++ mlog_project/static/js/add_genre.js
@@ -1,3 +1,15 @@
+var genre_create_popup_url = document.currentScript.getAttribute('genre_create_popup_url');
+console.log(genre_create_popup_url);
+
+function open_add_genre_window(){
+	console.log('called');
+	window.open(genre_create_popup_url, 'subwin_genre');
+	console.log('finished');
+}
+
+var add_genre_link = document.getElementById('add_genre_link');
+add_genre_link.addEventListener('click', open_add_genre_window, false);
+
 function add_genre(name, pk){
 	var select=document.getElementById('id_genre');
 	var option=document.createElement('option');
