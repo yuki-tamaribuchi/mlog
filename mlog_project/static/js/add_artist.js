19e20279f909e35cb19c2a245d8c465e18bfc008
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-08-16 11:51:10 +0900

    Move open add artist window function to add_artist.js

--- mlog_project/static/js/add_artist.js
+++ mlog_project/static/js/add_artist.js
@@ -1,3 +1,12 @@
+var add_artist_link_url = document.currentScript.getAttribute('add_artist_link_url');
+
+function open_add_artist_window(){
+	window.open(add_artist_link_url,'subwin_artist');
+}
+
+var add_artist_link = document.getElementById('add_artist_link');
+add_artist_link.addEventListener('click', open_add_artist_window, false);
+
 function add_artist(name, pk){
 	var select=document.getElementById('id_artist');
 	var option=document.createElement('option');
