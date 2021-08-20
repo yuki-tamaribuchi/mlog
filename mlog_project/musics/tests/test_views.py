3d0abeb8f9cc73bf6d33efc48e0e0dccb4dcbc71
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-08-20 10:24:30 +0900

    Create test_*_context_in for ArtistDetailViewTest

--- mlog_project/musics/tests/test_views.py
+++ mlog_project/musics/tests/test_views.py
@@ -17,3 +17,27 @@ class ArtistDetailViewTest(TestCase):
 		response = self.client.get(reverse('musics:artist_detail', kwargs={'slug':self.entry.slug}))
 		self.assertEqual(response.status_code, 200)
 		self.assertTemplateUsed('musics/artist_detail.html')
+
+	def test_entries_in_context(self):
+		response = self.client.get(reverse('musics:artist_detail', kwargs={'slug':self.entry.slug}))
+		context = response.context
+		self.assertEqual(response.status_code, 200)
+		self.assertIn('entries', context)
+
+	def test_members_in_context(self):
+		response = self.client.get(reverse('musics:artist_detail', kwargs={'slug':self.entry.slug}))
+		context = response.context
+		self.assertEqual(response.status_code, 200)
+		self.assertIn('members', context)
+	
+	def test_song_list_in_context(self):
+		response = self.client.get(reverse('musics:artist_detail', kwargs={'slug':self.entry.slug}))
+		context = response.context
+		self.assertEqual(response.status_code, 200)
+		self.assertIn('song_list', context)
+
+	def test_fav_status_in_context_not_loggedin(self):
+		response = self.client.get(reverse('musics:artist_detail', kwargs={'slug':self.entry.slug}))
+		context = response.context
+		self.assertEqual(response.status_code, 200)
+		self.assertNotIn('fav_status', context)
