431603156a5e87f4c4dae710e4af9ddabce83526
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-08-24 09:40:38 +0900

    Modify UserListByFavoritedArtistView's url path name to user_list_by_favorited_artist

--- mlog_project/favorite_artists/urls.py
+++ mlog_project/favorite_artists/urls.py
@@ -5,6 +5,6 @@ from .views import FavoriteArtistProcess, UserListByFavoritedArtistView, UserFav
 app_name='favorite_artists'
 urlpatterns=[
 	path('process/', FavoriteArtistProcess.as_view(), name='process'),
-	path('artist/<str:slug>/', UserListByFavoritedArtistView.as_view(), name='userfavorite'),
+	path('artist/<str:slug>/', UserListByFavoritedArtistView.as_view(), name='user_list_by_favorited_artist'),
 	path('user/<str:username>/', UserFavoriteArtistListView.as_view(), name='favoriteartist'),
 ]
