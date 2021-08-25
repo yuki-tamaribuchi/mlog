9fa961d38dc0e65207428e4b0efd0529f38b2dca
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-08-26 07:59:17 +0900

    Add url to follow_process

--- mlog_project/follow/urls.py
+++ mlog_project/follow/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import FollowProcess, FollowingListView, FollowerListView
+from .views import FollowProcess, FollowingListView, FollowerListView, follow_process
 
 app_name='follow'
 urlpatterns=[
+	path('follow/', follow_process, name='follow'),
 	path('process/', FollowProcess.as_view(), name='process'),
 	path('following/<str:username>/', FollowingListView.as_view(), name='following'),
 	path('follower/<str:username>/', FollowerListView.as_view(), name='follower'),
