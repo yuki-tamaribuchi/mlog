a9ec05fad1cd62e0901a6a7f417e9932d73f3b4f
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-08-25 09:09:30 +0900

    Create EntryListByArtistView

--- mlog_project/entry/views.py
+++ mlog_project/entry/views.py
@@ -9,7 +9,7 @@ from activity.models import EntryReadActivity
 from accounts.models import User
 from likes.models import Like
 from comments.models import Comment
-from musics.models import Song
+from musics.models import Artist, Song
 
 from .models import Entry
 from .forms import EntryForm
@@ -90,4 +90,29 @@ class EntryListBySongView(ListView):
 	def get_context_data(self, *args, **kwargs) :
 		context = super().get_context_data(**kwargs)
 		context['song'] = get_object_or_404(Song, id=self.kwargs.get('pk'))
+		return context
+
+
+class EntryListByArtistView(ListView):
+	model = Entry
+	template_name = 'entry/entry_list.html'
+	paginate_by = 12
+
+	def get_queryset(self):
+		qs = super().get_queryset()
+
+		try:
+			return qs.select_related(
+				'writer', 'song'
+			).prefetch_related(
+				'song__artist'
+			).filter(
+				song__artist__slug=self.kwargs.get('slug')
+			).order_by('id')
+		except ObjectDoesNotExist:
+			return qs.none()
+	
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['artist'] = get_object_or_404(Artist, slug=self.kwargs.get('slug'))
 		return context
