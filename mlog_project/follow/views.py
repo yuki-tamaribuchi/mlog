3851d72dfdd02068f03a2d9ceed08f363df90ae1
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-08-26 08:04:46 +0900

    Delete FollowProcess view

--- mlog_project/follow/views.py
+++ mlog_project/follow/views.py
@@ -9,23 +9,6 @@ from accounts.models import User
 from .models import Follow
 
 
-class FollowProcess(LoginRequiredMixin, View):
-
-	def post(self, *args, **kwargs):
-		user = User.objects.get(username=self.request.user.username)	
-		follow_user = User.objects.get(username=self.request.POST['username'])
-		following = Follow.objects.filter(user__username=user.username, follower__username=follow_user.username)
-
-		if user==follow_user:
-			return redirect(self.request.META['HTTP_REFERER'])
-		
-		if following.exists():
-			following.delete()
-		else:
-			Follow.objects.create(user=user, follower=follow_user)
-
-		return redirect(self.request.META['HTTP_REFERER'])
-
 def follow_process(request):
 
 	if request.method == 'POST':
