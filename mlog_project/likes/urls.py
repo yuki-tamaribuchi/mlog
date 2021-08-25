29f92c9be8e27aaa6efc00bbc41ffecc98fb7901
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-08-26 08:59:42 +0900

    Delete url to LikeProcess

--- mlog_project/likes/urls.py
+++ mlog_project/likes/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from .views import LikeProcess, EntrysLikeListView, UsersLikeListView, like_process
+from .views import EntrysLikeListView, UsersLikeListView, like_process
 
 app_name='likes'
 urlpatterns=[
 	path('like/', like_process, name='like'),
-	path('process/', LikeProcess.as_view(), name='process'),
 	path('userlist/<str:username>/', UsersLikeListView.as_view(), name='user_list'),
 	path('entrylist/<int:pk>/', EntrysLikeListView.as_view(), name='entry_list'),
 ]
