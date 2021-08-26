095d8c0859a76331d730baa6be3d4f958d790c47
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-08-26 09:29:16 +0900

    Add url to favorite_process

--- mlog_project/favorite_artists/urls.py
+++ mlog_project/favorite_artists/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import FavoriteArtistProcess, UserListByFavoritedArtistView, UserFavoritesArtistListView
+from .views import FavoriteArtistProcess, UserListByFavoritedArtistView, UserFavoritesArtistListView, favorite_process
 
 app_name='favorite_artists'
 urlpatterns=[
+	path('favorite/', favorite_process, name='favorite'),
 	path('process/', FavoriteArtistProcess.as_view(), name='process'),
 	path('artist/<str:slug>/', UserListByFavoritedArtistView.as_view(), name='user_list_by_favorited_artist'),
 	path('user/<str:username>/', UserFavoritesArtistListView.as_view(), name='user_favorites_artist_list'),
