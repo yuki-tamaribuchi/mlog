f6e01e0c792d3addc0b2ed5acac7e06778e72e2b
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-08-26 09:32:27 +0900

    Delete url to FavoriteArtistProcess

--- mlog_project/favorite_artists/urls.py
+++ mlog_project/favorite_artists/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from .views import FavoriteArtistProcess, UserListByFavoritedArtistView, UserFavoritesArtistListView, favorite_process
+from .views import UserListByFavoritedArtistView, UserFavoritesArtistListView, favorite_process
 
 app_name='favorite_artists'
 urlpatterns=[
 	path('favorite/', favorite_process, name='favorite'),
-	path('process/', FavoriteArtistProcess.as_view(), name='process'),
 	path('artist/<str:slug>/', UserListByFavoritedArtistView.as_view(), name='user_list_by_favorited_artist'),
 	path('user/<str:username>/', UserFavoritesArtistListView.as_view(), name='user_favorites_artist_list'),
 ]
