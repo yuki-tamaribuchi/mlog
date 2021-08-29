9326b1ab92cf28281ef77db554c0bb439a94b2af
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-08-30 08:50:03 +0900

    Add url to ContactThreadDeleteView

--- mlog_project/contacts/urls.py
+++ mlog_project/contacts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from contacts.views import CreateContactThreadAndConentView, ContactThreadDetailView, ContactContentUpdateView
+from contacts.views import CreateContactThreadAndConentView, ContactThreadDetailView, ContactContentUpdateView, ContactThreadDeleteView
 
 app_name = 'contacts'
 
@@ -8,4 +8,5 @@ urlpatterns = [
 	path('create/', CreateContactThreadAndConentView.as_view(), name='create'),
 	path('detail/<int:pk>/', ContactThreadDetailView.as_view(), name='detail'),
 	path('update/<int:pk>/', ContactContentUpdateView.as_view(), name='update'),
+	path('delete/<int:pk>/', ContactThreadDeleteView.as_view(), name='delete'),
 ]
