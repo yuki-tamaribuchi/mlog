f3bcfdc3dad8af77690767ece83ae858361a280d
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-08-30 08:44:37 +0900

    Add url to ContactThreadDetailView

--- mlog_project/contacts/urls.py
+++ mlog_project/contacts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from contacts.views import CreateContactThreadAndConentView
+from contacts.views import CreateContactThreadAndConentView, ContactThreadDetailView
 
 app_name = 'contacts'
 
 urlpatterns = [
 	path('create/', CreateContactThreadAndConentView.as_view(), name='create'),
+	path('detail/<int:pk>/', ContactThreadDetailView.as_view(), name='detail'),
 ]
