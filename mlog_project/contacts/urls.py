e72468b0836d5e210e8dedfae2f96f220b2233eb
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-08-30 08:51:43 +0900

    Add url to ContactListView

--- mlog_project/contacts/urls.py
+++ mlog_project/contacts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from contacts.views import CreateContactThreadAndConentView, ContactThreadDetailView, ContactContentUpdateView, ContactThreadDeleteView
+from contacts.views import CreateContactThreadAndConentView, ContactThreadDetailView, ContactContentUpdateView, ContactThreadDeleteView, ContactListView
 
 app_name = 'contacts'
 
@@ -9,4 +9,5 @@ urlpatterns = [
 	path('detail/<int:pk>/', ContactThreadDetailView.as_view(), name='detail'),
 	path('update/<int:pk>/', ContactContentUpdateView.as_view(), name='update'),
 	path('delete/<int:pk>/', ContactThreadDeleteView.as_view(), name='delete'),
+	path('list/', ContactListView.as_view(), name='list'),
 ]
