040b1f2a856d5bf529385c16fc901c5f5e0c10a3
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-08-30 08:30:53 +0900

    Create ContactContent

--- mlog_project/contacts/models.py
+++ mlog_project/contacts/models.py
@@ -26,3 +26,14 @@ class ContactThreads(models.Model):
 
 	def __str__(self):
 		return '%s by %s'%(self.category, self.user.username)
+
+
+class ContactContent(models.Model):
+
+	parent_thread = models.ForeignKey(ContactThreads, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	content = models.TextField()
+	datetime = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return 'Content from %s to %s by %s'%(self.user.username, self.parent_thread.category, self.parent_thread.user)
