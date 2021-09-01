0d35140efa69209b84f8eb2dd87e7899fc782ba3
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-09-02 08:32:11 +0900

    Modify fields name from datetime to created_at

--- mlog_project/contacts/models.py
+++ mlog_project/contacts/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 from accounts.models import User
 
 MLOG = 'MLG'
@@ -22,7 +21,7 @@ class ContactThreads(models.Model):
 
 	category = models.CharField(max_length=3, choices=CONTACT_CATEGORY_CHOICES)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	datetime = models.DateTimeField(auto_now=True)
+	created_at = models.DateTimeField(auto_now=True)
 
 	def __str__(self):
 		return '%s by %s'%(self.category, self.user.username)
@@ -33,7 +32,7 @@ class ContactContent(models.Model):
 	parent_thread = models.ForeignKey(ContactThreads, on_delete=models.CASCADE)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	content = models.TextField()
-	datetime = models.DateTimeField(auto_now=True)
+	created_at = models.DateTimeField(auto_now=True)
 
 	def __str__(self):
 		return 'Content from %s to %s by %s'%(self.user.username, self.parent_thread.category, self.parent_thread.user)
