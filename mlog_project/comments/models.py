9106fdea156baf391b5da3b9c997faedaa4c9916
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-09-03 08:33:43 +0900

    Add verbose_name for author in ja

--- mlog_project/comments/models.py
+++ mlog_project/comments/models.py
@@ -7,7 +7,7 @@ from entry.models import Entry
 class Comment(models.Model):
 	comment = models.TextField(verbose_name=_('comment'), max_length=200)
 	entry = models.ForeignKey(Entry, on_delete=models.CASCADE, verbose_name=_('entry'))
-	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_('author'))
 
 	def __str__(self):
 		return '%s comment to %s'%(self.user.username, self.entry.title)
