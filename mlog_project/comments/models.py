ddba7b9bd3fcebe41c3241921499f8aac48cf036
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-09-03 08:16:20 +0900

    Add verbose_name

--- mlog_project/comments/models.py
+++ mlog_project/comments/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from accounts.models import User
 from entry.models import Entry
 
 class Comment(models.Model):
-	comment = models.TextField(max_length=200)
+	comment = models.TextField(verbose_name=_('comment'), max_length=200)
 	entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 
