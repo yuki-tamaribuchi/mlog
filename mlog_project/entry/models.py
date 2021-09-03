79e404bc3872ad1c6550e4a3a19431631e0bee13
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-09-03 09:33:08 +0900

    Remove date and add created_at and updated_at

--- mlog_project/entry/models.py
+++ mlog_project/entry/models.py
@@ -8,7 +8,8 @@ class Entry(models.Model):
 	title = models.CharField(max_length=30)
 	content = models.TextField()
 	writer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='entrys_writer')
-	date = models.DateTimeField(auto_now_add=True)
+	created_at = models.DateTimeField()
+	updated_at = models.DateTimeField()
 	song = models.ForeignKey(Song, on_delete=models.PROTECT, related_name='featured_song')
 
 	def __str__(self):
