1ca80f7bd82ea10f1d042687a18e136e54000865
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-09-03 09:52:40 +0900

    Add verbose_name for FavoriteArtist

--- mlog_project/favorite_artists/models.py
+++ mlog_project/favorite_artists/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from accounts.models import User
 from musics.models import Artist
 
 class FavoriteArtist(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_('user'))
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, verbose_name=_('artist'))
 
     def __str__(self):
         return '%s favorite artist is %s'%(self.user.username, self.artist.artist_name)
