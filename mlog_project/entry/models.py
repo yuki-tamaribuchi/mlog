bcd03ec2497e85948754477d0639ad15ab77f71f
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-09-03 09:36:14 +0900

    Add save method

--- mlog_project/entry/models.py
+++ mlog_project/entry/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 from musics.models import Song
 from accounts.models import User
@@ -11,6 +12,12 @@ class Entry(models.Model):
 	created_at = models.DateTimeField()
 	updated_at = models.DateTimeField()
 	song = models.ForeignKey(Song, on_delete=models.PROTECT, related_name='featured_song')
+
+	def save(self, *args, **kwargs):
+		if not self.id:
+			self.created_at = timezone.now()
+		self.updated_at = timezone.now()
+		return super(Entry, self).save(*args, **kwargs)
 
 	def __str__(self):
 	    return '%s written by %s (Song:%s)'%(self.title, self.writer,self.song)
