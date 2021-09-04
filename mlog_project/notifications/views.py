aec5ddeab79484f8f01171d91c399804848fd2a0
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-09-05 08:46:14 +0900

    Create NotificationListView

--- mlog_project/notifications/views.py
+++ mlog_project/notifications/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import redirect
+from django.views.generic import ListView
 
 from notifications.tasks import notification_read
+from notifications.models import Notifications
 
 def notification_redirect(request, pk, username):
 	if request.method=='GET':
 		notification_read.delay(pk)
 		return redirect('accounts:detail', username=username)
+
+
+class NotificationListView(ListView):
+	model = Notifications
+	template_name = 'notifications/list.html'
+
+	def get_queryset(self):
+		qs = super().get_queryset()
+		return qs.filter(
+			user_to=self.request.user,
+		)
