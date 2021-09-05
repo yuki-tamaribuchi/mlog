fd88f5c85975b7edabdf97ff9b145bf126c7656e
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-09-06 08:43:53 +0900

    Add verbose_name for like model

--- mlog_project/likes/models.py
+++ mlog_project/likes/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from accounts.models import User
 from entry.models import Entry
 
 
 class Like(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_('user'))
+	entry = models.ForeignKey(Entry, on_delete=models.CASCADE, verbose_name=_('entry'))
 
 	def __str__(self):
 		return '%s liked %s'%(self.user,self.entry)
