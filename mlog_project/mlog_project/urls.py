22d4ec67aac9a94d7769ddd75292da38fb85df20
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-09-06 08:13:07 +0900

    Add url to i18n

--- mlog_project/mlog_project/urls.py
+++ mlog_project/mlog_project/urls.py
@@ -36,5 +36,6 @@ urlpatterns = [
     path('notifications/', include('notifications.urls'), name='notifications'),
     path('contacts/', include('contacts.urls'), name='contacts'),
     path('select2/', include('django_select2.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
     path('__debug__/', include(debug_toolbar.urls)),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
