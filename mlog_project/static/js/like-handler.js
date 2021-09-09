d5f9640943681bc1e290523ece4f3c833ff91025
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-09-10 08:09:13 +0900

    Modify process for not logged in

--- mlog_project/static/js/like-handler.js
+++ mlog_project/static/js/like-handler.js
@@ -1,24 +1,40 @@
 var like_url = document.currentScript.getAttribute('like_url');
-var liked_entry = document.currentScript.getAttribute('liked_entry');
+var entry_id = document.currentScript.getAttribute('entry_id');
 var like_btn = document.getElementById('like-btn');
+var is_loggedin = document.currentScript.getAttribute('is_loggedin');
 
 like_btn.addEventListener('click', like_process, false);
 
 function like_process(){
-	$.ajax({
-		'url':like_url,
-		'type':'POST',
-		'data':{
-			'liked_entry':liked_entry
-		},
-		'dataType':'json'
-	})
-	.done(function(response){
-		like_status = response['like_status'];
-		if (like_status){
-			like_btn.innerHTML = '<i class="fa fa-heart like-true"></i>';
-		}else{
-			like_btn.innerHTML = '<i class="fa fa-heart like-false"></i>';
-		}
-	});
+	if(is_loggedin==='True'){
+		$.ajax({
+			'url':like_url,
+			'type':'POST',
+			'data':{
+				'entry_id':entry_id
+			},
+			'dataType':'json'
+		})
+		.done(function(response){
+			like_status = response['like_status'];
+			if (like_status){
+				like_btn.innerHTML = '<i class="fa fa-heart like-true"></i>';
+			}else{
+				like_btn.innerHTML = '<i class="fa fa-heart like-false"></i>';
+			}
+		});
+	}else{
+		$.ajax({
+			'url':like_url,
+			'type':'GET',
+			'data':{
+				'entry_id':entry_id
+			},
+			'dataType':'json'
+		})
+		.done(function(response){
+			console.log(response['login_url']);
+			window.location.replace(response['login_url']);
+		});
+	}
 }
