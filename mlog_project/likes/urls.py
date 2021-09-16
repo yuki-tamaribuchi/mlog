383208653d79103cf36b4232150ed23f9257901d
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-09-16 10:21:40 +0900

    Rename EntryLikedUserListView path to entry_liked_user_list

--- mlog_project/likes/urls.py
+++ mlog_project/likes/urls.py
@@ -6,5 +6,5 @@ app_name='likes'
 urlpatterns=[
 	path('like/', like_process, name='like'),
 	path('userlist/<str:username>/', UserLikedEntryListView.as_view(), name='user_liked_entry_list'),
-	path('entrylist/<int:pk>/', EntryLikedUserListView.as_view(), name='entry_list'),
+	path('entrylist/<int:pk>/', EntryLikedUserListView.as_view(), name='entry_liked_user_list'),
 ]
