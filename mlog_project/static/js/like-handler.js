34ba87b1b6975e36020b343ad6e8ae967debd748
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-09-16 09:47:29 +0900

    Add like count update function

--- mlog_project/static/js/like-handler.js
+++ mlog_project/static/js/like-handler.js
@@ -2,6 +2,7 @@ var like_url = document.currentScript.getAttribute('like_url');
 var entry_id = document.currentScript.getAttribute('entry_id');
 var like_btn = document.getElementById('like-btn');
 var is_loggedin = document.currentScript.getAttribute('is_loggedin');
+var liked_user_list_link = document.getElementById('liked_user_list_link');
 
 like_btn.addEventListener('click', like_process, false);
 
@@ -17,11 +18,13 @@ function like_process(){
 		})
 		.done(function(response){
 			like_status = response['like_status'];
+			like_count = response['like_count'];
 			if (like_status){
 				like_btn.innerHTML = '<i class="fa fa-heart like-true"></i>';
 			}else{
 				like_btn.innerHTML = '<i class="fa fa-heart like-false"></i>';
 			}
+			liked_user_list_link.innerHTML = "Like:"+like_count;
 		});
 	}else{
 		$.ajax({
