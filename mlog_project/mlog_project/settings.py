8510dd38c15b80c55e7d65fe1731e0d8ed80c08a
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-09-17 08:56:48 +0900

    Delete AUTHENTICATION_BACKENDS from settings

--- mlog_project/mlog_project/settings.py
+++ mlog_project/mlog_project/settings.py
@@ -176,11 +176,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
-]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
