afb72a5b5eb18f99037b5b64d3c04011253d6155
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-09-17 08:08:31 +0900

    Devide file for django-nose settings

--- mlog_project/mlog_project/settings.py
+++ mlog_project/mlog_project/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 import os
 
 from settings_csp import *
+from settings_nose import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -297,24 +298,6 @@ CELERY_RESULT_BACKEND = CELERY_RESULT_BACKENDS[os.environ.get('CELERY_RESULT_BAC
 CELERY_TIMEZONE = "Asia/Tokyo"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
-
-
-#Setting for django-nose
-#TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-
-NOSE_ARGS_LIST = {
-    'local':{
-        '--with-xunit',
-        '--xunit-file=result/unittest.xml',
-    },
-    'docker':{
-        '--with-xunit',
-        '--xunit-file=mlog_project/result/unittest.xml',
-    }
-}
-
-nose_args_selection = os.environ.get('NOSE_ARGS_SELECTION', 'local')
-NOSE_ARGS = NOSE_ARGS_LIST[nose_args_selection]
 
 
 SPOTIFY_CLIENT_ID = os.environ.get('MLOG_SPOTIFY_CLIENT_ID')
