fcd708a4524e9c0699620c70dd3bea1f8e6faed0
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2021-09-17 08:07:33 +0900

    Devide file for CSP settings

--- mlog_project/mlog_project/settings.py
+++ mlog_project/mlog_project/settings.py
@@ -14,6 +14,8 @@ from django.utils.translation import gettext_lazy as _
 from pathlib import Path
 import os
 
+from settings_csp import *
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -313,51 +315,6 @@ NOSE_ARGS_LIST = {
 
 nose_args_selection = os.environ.get('NOSE_ARGS_SELECTION', 'local')
 NOSE_ARGS = NOSE_ARGS_LIST[nose_args_selection]
-
-
-CSP_DEFAULT_SRC = [
-    "'self'",
-    "https://ka-f.fontawesome.com",
-    "https://p.scdn.co",
-    ]
-CSP_SCRIPT_SRC = [
-    "'self'",
-    "https://code.jquery.com",
-    "https://stackpath.bootstrapcdn.com",
-    "https://cdnjs.cloudflare.com",
-    "https://cdn.jsdelivr.net",
-    "https://kit.fontawesome.com",
-    "https://mlog-s3.s3.amazonaws.com",
-    "https://d3uz1xyl9ewfg.cloudfront.net",
-]
-CSP_STYLE_SRC = [
-    "'self'",
-    "'unsafe-inline'",
-    "https://stackpath.bootstrapcdn.com",
-    "https://cdn.jsdelivr.net",
-    "https://cdnjs.cloudflare.com",
-    "https://mlog-s3.s3.amazonaws.com",
-    "https://d3uz1xyl9ewfg.cloudfront.net",
-    ]
-CSP_IMG_SRC = [
-    "'self'",
-    "https://storage.googleapis.com",
-    "https://i.scdn.co",
-    "https://mlog-s3.s3.amazonaws.com",
-    "https://d3uz1xyl9ewfg.cloudfront.net",
-]
-CSP_FONT_SRC = [
-    "'self'",
-    "https://cdnjs.cloudflare.com",
-    "https://ka-f.fontawesome.com",
-]
-CSP_FRAME_SRC = [
-    "https://*.spotify.com",
-]
-
-CSP_MEDIA_SRC = [
-    "https://p.scdn.co",
-]
 
 
 SPOTIFY_CLIENT_ID = os.environ.get('MLOG_SPOTIFY_CLIENT_ID')
---
+++ mlog_project/mlog_project/settings_csp.py
@@ -0,0 +1,43 @@
+CSP_DEFAULT_SRC = [
+    "'self'",
+    "https://ka-f.fontawesome.com",
+    "https://p.scdn.co",
+    ]
+CSP_SCRIPT_SRC = [
+    "'self'",
+    "https://code.jquery.com",
+    "https://stackpath.bootstrapcdn.com",
+    "https://cdnjs.cloudflare.com",
+    "https://cdn.jsdelivr.net",
+    "https://kit.fontawesome.com",
+    "https://mlog-s3.s3.amazonaws.com",
+    "https://d3uz1xyl9ewfg.cloudfront.net",
+]
+CSP_STYLE_SRC = [
+    "'self'",
+    "'unsafe-inline'",
+    "https://stackpath.bootstrapcdn.com",
+    "https://cdn.jsdelivr.net",
+    "https://cdnjs.cloudflare.com",
+    "https://mlog-s3.s3.amazonaws.com",
+    "https://d3uz1xyl9ewfg.cloudfront.net",
+    ]
+CSP_IMG_SRC = [
+    "'self'",
+    "https://storage.googleapis.com",
+    "https://i.scdn.co",
+    "https://mlog-s3.s3.amazonaws.com",
+    "https://d3uz1xyl9ewfg.cloudfront.net",
+]
+CSP_FONT_SRC = [
+    "'self'",
+    "https://cdnjs.cloudflare.com",
+    "https://ka-f.fontawesome.com",
+]
+CSP_FRAME_SRC = [
+    "https://*.spotify.com",
+]
+
+CSP_MEDIA_SRC = [
+    "https://p.scdn.co",
+]
